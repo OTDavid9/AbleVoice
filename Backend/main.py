886bf3e04008154d54d5bd28dc063f7873b86774
Obidokun Tunji <tunjiobidokun@gmail.com>
2024-03-23 15:44:23 -0700

    Create backend api for the project and folder structure

--- Backend/main.py
+++ Backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import os
 
@@ -10,6 +11,12 @@ templates_dir = os.path.join(os.path.dirname(__file__), "../templates")
 
 # Initialize Jinja2Templates instance
 templates = Jinja2Templates(directory=templates_dir)
+
+# Get the path to the static folder
+static_dir = os.path.join(os.path.dirname(__file__), "../static")
+
+# Mount the static directory
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 # Define route handlers
 @app.get("/", response_class=HTMLResponse)
